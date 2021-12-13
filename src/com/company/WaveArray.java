f65c22fa69badcde88cf19bc688f8e83c4209566
Pawan Kumar <pawancse.16@gmail.com>
2021-12-13 21:49:46 +0100

    #noissue : Sort the array into a wave like array.

--- src/com/company/WaveArray.java
+++ src/com/company/WaveArray.java
@@ -1,4 +1,14 @@
 package com.company;
 
 public class WaveArray {
+
+    public static void convertToWave(int arr[], int n){
+        int temp = 0;
+        for(int i = 0; i<arr.length;i+=2){
+            temp = arr[i];
+            arr[i] = arr[i+1];
+            arr[i+1] = temp;
+        }
+    }
+
 }
